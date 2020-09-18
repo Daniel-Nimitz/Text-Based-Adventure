1555d4ca0314f0e3eee090ed005e0c087d486958
Daniel-Nimitz <63825442+Daniel-Nimitz@users.noreply.github.com>
2020-09-18 14:50:07 -0600

    initial commit

---
+++ Assets/Scripts/OpenMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenMenu : MonoBehaviour
+{
+    [SerializeField] GameObject MenuCanvas;
+
+    //This will open the menu
+    public void ActivateMenu() {
+        MenuCanvas.SetActive(true);
+    }
+    //this will close the menu
+    public void CloseMenu()
+    {
+        MenuCanvas.SetActive(false);
+    }
+
+    //this will quit the game
+    public void QuitGame() {
+        Application.Quit();
+    }
+
+}
---
+++ Assets/Scripts/TextChange.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class TextChange : MonoBehaviour
+{
+    public Text mainText;
+    public Button ButtonOne;
+    public Button ButtonTwo;
+
+    public bool optionOne;
+    public bool optionTwo;
+
+
+    public void clickOne() {
+        mainText.text = "You have pressed the first button";
+    }
+
+    public void clickTwo() {
+        mainText.text = "You have pressed the second button";
+    }
+}
