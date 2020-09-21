04796232908fa80b631edda896404aee95ec9e63
Daniel-Nimitz <63825442+Daniel-Nimitz@users.noreply.github.com>
2020-09-21 15:27:21 -0600

    Created new Page, begin working on script for changing pages

--- Assets/Scripts/TextChange.cs
+++ Assets/Scripts/TextChange.cs
@@ -1,24 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
 using UnityEngine.UI;
 
 public class TextChange : MonoBehaviour
 {
     public Text mainText;
+
     public Button ButtonOne;
     public Button ButtonTwo;
+    public Button ButtonThree;
+    public Button ButtonFour;
 
-    public bool optionOne;
-    public bool optionTwo;
+    public Page currentPage;
 
+    //Maybe we can use enums to make each button be generated faster
 
-    public void clickOne() {
-        mainText.text = "You have pressed the first button";
-    }
+    public void ClickButtonOne() {
+        currentPage = currentPage.buttonOnePageTo;
+        mainText.text = currentPage.buttonOnePageTo.description;
+        
+        //here we turn buttons to active or inactive
+        ButtonOne.gameObject.SetActive(currentPage.buttonOnePageTo.buttonOneVisible);
+        ButtonTwo.gameObject.SetActive(currentPage.buttonOnePageTo.buttonTwoVisible);
+        ButtonThree.gameObject.SetActive(currentPage.buttonOnePageTo.buttonThreeVisible);
+        ButtonFour.gameObject.SetActive(currentPage.buttonOnePageTo.buttonFourVisible);
 
-    public void clickTwo() {
-        mainText.text = "You have pressed the second button";
+        //Here we update the text for buttons
+        ButtonOne.GetComponentInChildren<Text>().text = currentPage.buttonOnePageTo.buttonOneText;
+        ButtonTwo.GetComponentInChildren<Text>().text = currentPage.buttonOnePageTo.buttonTwoText;
+        ButtonThree.GetComponentInChildren<Text>().text = currentPage.buttonOnePageTo.buttonThreeText;
+        ButtonFour.GetComponentInChildren<Text>().text = currentPage.buttonOnePageTo.buttonFourText;
     }
 }
