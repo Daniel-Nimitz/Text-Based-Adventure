aef6f8fd1eb89a38590c91357bb6acf30da2f7c2
Daniel-Nimitz <63825442+Daniel-Nimitz@users.noreply.github.com>
2020-11-19 21:28:48 -0700

    SimpleLoad.cs loads base stats Load button added for testing purpouses

--- Assets/Scripts/SimpleLoad.cs
+++ Assets/Scripts/SimpleLoad.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class SimpleLoad : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject statTrackerObject;
+    public GameObject textChangeObject;
 
-    // Update is called once per frame
-    void Update()
+    public void Load()
     {
-        
+        //Here we are taking the objects which have the scripts we want to reference and finding the scripts to reference from those objects
+        StatTracker statTrackerScript = statTrackerObject.GetComponent<StatTracker>();
+        TextChange textChangeScript = textChangeObject.GetComponent<TextChange>();
+        statTrackerScript.strength = ES3.Load<int>("strength");
+        statTrackerScript.corruption = ES3.Load<int>("corruption");
+        statTrackerScript.arcana = ES3.Load<int>("arcana");
+        Debug.Log("The Load Function was called");
     }
 }
