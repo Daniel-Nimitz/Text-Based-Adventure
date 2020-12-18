96c4e112b4d71c3f657cba475e543a7094d99942
Daniel-Nimitz <63825442+Daniel-Nimitz@users.noreply.github.com>
2020-12-18 13:46:49 -0700

    This is a comit made so thaat I can show a scrollbar resizing issue to stack overflow

---
+++ Assets/Scripts/ScrollbarFix.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollbarFix : MonoBehaviour
+{
+   
+        public Scrollbar scrollbar;
+        public ScrollRect scrollRect;
+    // Use this for initialization
+    private void Awake()
+    {
+        scrollRect.horizontalNormalizedPosition = 0.4f;
+
+    }
+    public void ChangeScrollPos()
+        {
+            scrollRect.horizontalNormalizedPosition = scrollbar.value;
+        }
+    }
