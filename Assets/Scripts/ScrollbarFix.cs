07c345b16717eb1284df7b8783f6b8cf145a839e
Daniel-Nimitz <63825442+Daniel-Nimitz@users.noreply.github.com>
2021-01-07 13:00:55 -0700

    Scrollbar doesnt cover words in Adventure 1

---
+++ Assets/Scripts/ScrollbarFix.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollbarFix : MonoBehaviour
+{
+
+    public Scrollbar scrollbar;
+    public ScrollRect scrollRect;
+    // Use this for initialization
+    private void Update()
+    {
+        scrollbar.size = 0.2f;
+        scrollRect.horizontalNormalizedPosition = 0.4f;
+
+    }
+
+    public void ChangeScrollPos()
+    {
+        scrollRect.horizontalNormalizedPosition = scrollbar.value;
+    }
+}
